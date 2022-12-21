9a16f026ddcc1da6b17facffae015dccd2f56e14
Gusta1075 <gustavosanmartino.gs@gmail.com>
2022-12-21 01:05:43 +0100

    arreglos

--- assets/js/mov.js
+++
@@ -1,9 +0,0 @@
-const desplazarArriba = document.querySelector("#desplazarse-hacia-arriba");
-
-desplazarArriba.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
